2b490f540946b2e18bbe0664d9ddf8052545aa29
Baldre <karljbalder@gmail.com>
2023-12-14 17:01:13 +0200

    Initial commit

---
+++ src/test/java/ee/balder/filters/FiltersApplicationTests.java
@@ -0,0 +1,15 @@
+package ee.balder.filters;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltersApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
