61b6115a7d2fe844bb310cca51b7a035ba6b18d9
Baldre <karljbalder@gmail.com>
2023-12-25 21:33:04 +0200

    Added CORS mapping for client communication

--- src/main/java/ee/balder/filters/FiltersApplication.java
+++ src/main/java/ee/balder/filters/FiltersApplication.java
@@ -2,12 +2,26 @@ package ee.balder.filters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FiltersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FiltersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FiltersApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/filter/**")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:8080");
+            }
+        };
+    }
 
 }
